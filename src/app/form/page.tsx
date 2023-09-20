14d888b003a74b2e1e4279aa6d2cd297019061f5
Filipe Silva <filipess671@gmail.com>
2023-09-20 04:23:15 -0300

    Form finalizado

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Button } from "antd";
+import { Button, Checkbox } from "antd";
+import type { CheckboxChangeEvent } from "antd/es/checkbox";
 import styles from "./page.module.css";
 
 const HomePage = () => (
@@ -12,15 +13,86 @@ const HomePage = () => (
     </header>
     <main className={styles.main}>
       <h1>Formulário</h1>
-      <form>
+      <form className={styles.form}>
         <div className={styles.divInput}>
           <label htmlFor="implementosAgricolas">Implementos Agrícolas</label>
-          <input type="text" placeholder="2 Arados, 2 Subsolador" />
+          <input
+            type="text"
+            placeholder="2 Arados, 2 Subsolador"
+            name="implementosAgricolas"
+            id="implementosAgricolas"
+          />
+        </div>
+
+        <div className={styles.divInput}>
+          <label htmlFor="areaPlantação">Área Plantação(he)</label>
+          <input
+            type="text"
+            placeholder="50"
+            name="implementosAgricolas"
+            id="implementosAgricolas"
+          />
+        </div>
+
+        <div className={styles.divInput}>
+          <label htmlFor="areaMataNativa">Área Mata Nativa</label>
+          <input
+            type="text"
+            placeholder="10"
+            name="implementosAgricolas"
+            id="implementosAgricolas"
+          />
+        </div>
+
+        <div className={styles.divInput}>
+          <label htmlFor="beneficiamentoCafe">
+            Beneficiamento de Café (Kg)
+          </label>
+          <input
+            type="text"
+            placeholder="20"
+            name="implementosAgricolas"
+            id="implementosAgricolas"
+          />
         </div>
         <div className={styles.divInput}>
-          <label htmlFor="areaDesmatada"></label>
-          <input type="text" placeholder="" />
+          <label htmlFor="areaDesmatamento">Área Desmatamento(he)</label>
+          <input
+            type="text"
+            placeholder="10"
+            name="areaDesmatamento"
+            id="areaDesmatamento"
+          />
         </div>
+
+        <div className={styles.divInput}>
+          <label htmlFor="areaPecuaria">Área Pecuária</label>
+          <input
+            type="text"
+            placeholder="10"
+            name="areaPecuaria"
+            id="areaPecuaria"
+          />
+        </div>
+
+        <div className={styles.divInput}>
+          <label htmlFor="fotovoltaico">Fotovoltaico</label>
+          <input
+            type="text"
+            placeholder="Não"
+            name="fotovoltaico"
+            id="fotovoltaico"
+          />
+        </div>
+
+        <div className={styles.divInput}>
+          <label htmlFor="areaGrama">Área Grama(he)</label>
+          <input type="text" placeholder="30" name="areaGrama" id="areaGrama" />
+        </div>
+
+        <Button type="primary" href="/result">
+          Submit
+        </Button>
       </form>
     </main>
   </section>
